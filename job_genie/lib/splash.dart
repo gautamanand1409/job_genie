9e560578b860732a84b08612cf0c691b0defd236
GAUTAM ANAND <gautamanand1409@gmail.com>
2022-05-12 16:14:01 +0530

    commit

---
+++ job_genie/lib/splash.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Container(
+        child: Text(
+          'JOB',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+      ),
+    ));
+  }
+}
