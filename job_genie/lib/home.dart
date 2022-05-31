4706c2bdd808306b2d1e49fdbabc9cb7e71c85e0
GAUTAM ANAND <gautamanand1409@gmail.com>
2022-05-31 15:55:25 +0530

    .

--- job_genie/lib/home.dart
+++ job_genie/lib/home.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:job_genie/navigation_drawer.dart';
 
 class MyHome extends StatelessWidget {
-  const MyHome({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +17,143 @@ class MyHome extends StatelessWidget {
               const BorderRadius.vertical(bottom: const Radius.circular(16)),
         ),
         elevation: 0,
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: <Widget>[
+          Card(
+            color: Color.fromARGB(255, 200, 227, 249),
+            child: Stack(
+              children: <Widget>[
+                Center(
+                  child: Icon(
+                    Icons.wifi,
+                    size: 50,
+                    color: Colors.black,
+                  ), //itha image use kar konte pan tujhya hishobane
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 160, left: 73),
+                  child: Text('Wi-Fi',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold)),
+                )
+              ],
+            ),
+          ),
+          Card(
+            color: Colors.green,
+            child: Stack(
+              children: <Widget>[
+                Center(
+                  child: Icon(
+                    Icons.devices,
+                    size: 50,
+                    color: Colors.white,
+                  ), //itha image use kar konte pan tujhya hishobane
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 140, left: 70),
+                  child: Text('Connected Devices',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold)),
+                )
+              ],
+            ),
+          ),
+          Card(
+            color: Colors.orange,
+            child: Stack(
+              children: <Widget>[
+                Center(
+                  child: Icon(
+                    Icons.access_alarm,
+                    size: 50,
+                    color: Colors.white,
+                  ), //itha image use kar konte pan tujhya hishobane
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 160, left: 100),
+                  child: Text('Clock',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold)),
+                )
+              ],
+            ),
+          ),
+          Card(
+            color: Colors.orange,
+            child: Stack(
+              children: <Widget>[
+                Center(
+                  child: Icon(
+                    Icons.access_alarm,
+                    size: 50,
+                    color: Colors.white,
+                  ), //itha image use kar konte pan tujhya hishobane
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 160, left: 100),
+                  child: Text('Clock',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold)),
+                )
+              ],
+            ),
+          ),
+          Card(
+            color: Colors.orange,
+            child: Stack(
+              children: <Widget>[
+                Center(
+                  child: Icon(
+                    Icons.access_alarm,
+                    size: 50,
+                    color: Colors.white,
+                  ), //itha image use kar konte pan tujhya hishobane
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 160, left: 100),
+                  child: Text('Clock',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold)),
+                )
+              ],
+            ),
+          ),
+          Card(
+            color: Colors.orange,
+            child: Stack(
+              children: <Widget>[
+                Center(
+                  child: Icon(
+                    Icons.access_alarm,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 160, left: 100),
+                  child: Text('Clock',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold)),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
